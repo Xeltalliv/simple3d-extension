42c21c138ccac0de048d4fc89741c1b1e9551c80
Xeltalliv <vadim@vvv.kiev.ua>
2024-06-30 22:28:37 +0300

    Highlighting and Javascript warning

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
+const hash = location.hash.substring(1);
 const main = document.getElementById("content");
+main.textContent = "";
 for(const category of links) {
+	const content = document.createElement("a");
+	content.id = category.dir;
+	content.classList.add("content");
+	if (hash == category.dir) content.classList.add("content-focus");
 	const header = document.createElement("h2");
+	header.style.margin = "0";
 	header.textContent = category.name;
-	main.append(header);
+	content.append(header);
+	const footer = document.createElement("br");
+	main.append(content, footer);
 	for(const link of category.links) {
 		const a = document.createElement("a");
 		a.href = `https://turbowarp.org/editor?project_url=${location.href}projects/${category.dir}/${link.file}.sb3`;
 		a.textContent = link.title;
 		a.target = "_blank";
 		a.rel = "noopener noreferrer";
+		a.id = `${category.dir}/${link.file}`;
+		if (hash == `${category.dir}/${link.file}`) a.classList.add("a-focus");
 		const br = document.createElement("br");
-		main.append(a, br);
+		content.append(a, br);
 	}
 	if (category.links.length == 0) {
 		const span = document.createElement("span");
 		span.textContent = "No examples in this section";
 		const br = document.createElement("br");
-		main.append(span, br);
+		content.append(span, br);
 	}
 }
